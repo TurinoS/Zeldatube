564fdc0674d037c547ff627b66a8f6d2b4b1194a
Paulo Turino <paulo.turino01@gmail.com>
2023-06-05 14:42:01 -0300

    adicionando o icone no header e na aba

--- zeldatube/src/Header/index.js
+++ zeldatube/src/Header/index.js
@@ -1,10 +1,11 @@
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import triforce from './triforce.png'
 
 export default function Header() {
     return(
         <header className={styles.header}>
-            <Link to='/'><img src='#' alt='Triforce' /></Link>
+            <Link to='/'><img src={triforce} alt='Triforce' /></Link>
             <div>
                 <Link to="/" >Página inicial</Link>
                 <Link to="/games" >Todos os jogos</Link>
